655453d16b39edc47230b274419200d34f0a3489
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2016-07-09 11:10:31 +0700

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,47 @@
 var path = require('path')
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	entry: ['./src/index'],
 	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		path: path.join(__dirname, 'dist')
 	},
 	plugins: [
 		new CopyWebpackPlugin([
-				{from: 'vendor/blockly/blockly_compressed.js'},
-				{from: 'vendor/blockly/blocks_compressed.js'},
-				{from: 'vendor/blockly/msg/js/en.js'},
-				{from: 'src/index.html'}
-			])
-		]
+				{from: 'vendor/blockly/blockly_compressed.js', to: 'assets/blockly/'},
+				{from: 'vendor/blockly/blocks_compressed.js', to: 'assets/blockly/'},
+				{from: 'vendor/blockly/msg/js/en.js', to: 'assets/blockly/'},
+		// 		// {from: 'src/index.html'}
+			]),
+		new HtmlWebpackPlugin({
+      			template: './src/index.html'
+    		})
+		],
+	module: {
+		// loaders: [{
+		// // {test: regex, 'loader-name'},			
+		// }]
+	},
+
 }
+
+
+// module.exports = {
+//     entry: "./entry.js",
+//     output: {
+//         path: __dirname,
+//         filename: "bundle.js"
+//     },
+//     plugins: [
+// 		new HtmlWebpackPlugin({
+//       			template: './src/index.html'
+// 		})
+//     ],
+//     module: {
+//         loaders: [
+//             { test: /\.css$/, loader: "style!css" }
+//         ]
+//     }
+// };
